bc538dea2776f08f770f1a077107a5266a0c7dad
ninja03 <69106571+ninja03@users.noreply.github.com>
2023-02-01 20:55:17 +0900

    Update BoardUpdate2.server.tsx

--- src/server/BoardUpdate2.server.tsx
+++ src/server/BoardUpdate2.server.tsx
@@ -2,7 +2,6 @@ import { HttpService, ReplicatedStorage } from "@rbxts/services";
 import Roact from '@rbxts/roact';
 
 const ReplicatedStorage = game.GetService("ReplicatedStorage");
-const Roact = require(ReplicatedStorage.Roact);
 
 const BoardManager = require(script.Parent.ModuleScript)
 
@@ -39,51 +38,51 @@ function access() {
 }
 
 function Board(data: any) {
-	if (!data.gaming && not data.ending) {
+	if (!data.gaming && !data.ending) {
 		return Roact.createFragment({})
 	}
-	local cells = {}
-	local width = data.board.width
-	local height = data.board.height
-	local xs = 1 / width
-	local ys = 1 / height
+	const cells = {};
+	const width = data.board.width;
+	const height = data.board.height;
+	const xs = 1 / width;
+	const ys = 1 / height;
 	
 	BoardManager.update(data)
 	
-	local layout = Roact.createElement("UIGridLayout", {
+	const layout = Roact.createElement("UIGridLayout", {
 		FillDirectionMaxCells = width,
 		SortOrder = "LayoutOrder",
 		CellSize = UDim2.new(xs, 0, ys, 0),
 		CellPadding = UDim2.new(0, 0, 0, 0) 
 	})
-	table.insert(cells, layout)
+	table.insert(cells, layout);
 	
-	for y = 1, height do
-		for x = 1, width do
-			local n = (y - 1) * width + x
-			local point = data.board.points[n]
-			local tile = data.tiled[n]
-			local playerImage, cellColor
-			if tile.player ~= nil then
-				local player = PLAYER[tile.player + 1] 
-				playerImage = player.image
-				cellColor = Color3.fromHex(player.color[tile.type + 1])
-			else
-				cellColor = FREE_COLOR
-			end
+	for (let y = 1; y < height; y++) {
+		for (let x = 1; x < width; x++) {
+			const n = (y - 1) * width + x;
+			const point = data.board.points[n];
+			const tile = data.tiled[n];
+			const playerImage, cellColor
+			if (tile.player != null) {
+				const player = PLAYER[tile.player + 1];
+				playerImage = player.image;
+				cellColor = Color3.fromHex(player.color[tile.type + 1]);
+			} else {
+				cellColor = FREE_COLOR;
+			}
 			
-			local cellContents = {} 
+			const cellContents = [];
 			
-			if playerImage then
-				local cellImage = Roact.createElement("ImageLabel", {
+			if (playerImage) {
+				const cellImage = Roact.createElement("ImageLabel", {
 					Image = playerImage,
 					BackgroundTransparency = 1,
 					Size = UDim2.new(1, 0, 1, 0)
 				})
 				table.insert(cellContents, cellImage)
-			end
+			}
 			
-			local cellText = Roact.createElement("TextLabel", {
+			const cellText = Roact.createElement("TextLabel", {
 				Text = point,
 				Size = UDim2.new(1, 0, 1, 0),
 				TextScaled = true,
@@ -94,13 +93,13 @@ function Board(data: any) {
 			})
 			table.insert(cellContents, cellText)
 			
-			local cell = Roact.createElement("Frame", {
+			const cell = Roact.createElement("Frame", {
 				BackgroundColor3 = cellColor,
 				LayoutOrder = n
 			}, cellContents)
 			table.insert(cells, cell)
-		end
-	end
+		}
+	}
 
 	return Roact.createElement("Frame", {
 			Position = UDim2.new(0, 0, 0, 400),
@@ -111,25 +110,24 @@ function Board(data: any) {
 }
 
 function Status(data: any) {
-	local text = ""
+	let text = ""
 	
-	if data.gaming or data.ending then
-		local player1 = data.players[1]
-		local player2 = data.players[2]
-		local point1 = player1.point.areaPoint + player1.point.wallPoint
-		local point2 = player2.point.areaPoint + player2.point.wallPoint
-		text = "ポイント:" .. point1 .. "対" .. point2 .. " ターン:" .. data.turn .. "/" .. data.totalTurn
-	end
+	if (data.gaming || data.ending) {
+		const player1 = data.players[1];
+		const player2 = data.players[2];
+		const point1 = player1.point.areaPoint + player1.point.wallPoint;
+		const point2 = player2.point.areaPoint + player2.point.wallPoint;
+		text = "ポイント:" + point1 + "対" + point2 + " ターン:" + data.turn + "/" + data.totalTurn;
+	}
 
 	return Roact.createElement("TextLabel", {
-		Position = UDim2.new(0, 0, 0, 0),
-		Size = UDim2.new(1, 0, 0, 200),
+		Position = new UDim2(0, 0, 0, 0),
+		Size = new UDim2(1, 0, 0, 200),
 		Text = text,
 		TextScaled = true,
-		BackgroundColor3 = Color3.new(1, 1, 1)
+		BackgroundColor3 = new Color3(1, 1, 1)
 	})
-end
-
+}
 
 function getUser(userId: string) {
 	const usersURL = "https://api.kakomimasu.com/v1/users/" + userId;
@@ -142,32 +140,32 @@ function getUser(userId: string) {
 }
 
 
-function PlayerName(data) {
-	local text = ""
+function PlayerName(data: any) {
+	let text = "";
 	
-	local player1Name = ""
-	local player2Name = ""
+	let player1Name = "";
+	let player2Name = "";
 	
-	if data.gaming or data.ending then
-		local player1 = data.players[1]
-		local player2 = data.players[2]
+	if (data.gaming || data.ending) {
+		const player1 = data.players[1];
+		const player2 = data.players[2];
 		
-		if (player1.type == "account") then
-			-- account player
-			local user1 = getUser(player1.id)
-			player1Name = user1.screenName
-			local user2 = getUser(player2.id)
-			player2Name = user2.screenName
-		else
-			-- guest player
+		if (player1.type == "account") {
+			// account player
+			const user1 = getUser(player1.id);
+			player1Name = user1.screenName;
+			const user2 = getUser(player2.id);
+			player2Name = user2.screenName;
+		} else {
+			// guest player
 			player1Name = player1.id
 			player2Name = player2.id
-		end
+		}
 		
-		local point1 = player1.point.areaPoint + player1.point.wallPoint
-		local point2 = player2.point.areaPoint + player2.point.wallPoint
-		text = player1Name .. "対" .. player2Name
-	end
+		const point1 = player1.point.areaPoint + player1.point.wallPoint;
+		const point2 = player2.point.areaPoint + player2.point.wallPoint;
+		text = player1Name + "対" + player2Name
+	}
 
 	return Roact.createElement("TextLabel", {
 		Position = UDim2.new(0, 0, 0, 200),
@@ -178,7 +176,7 @@ function PlayerName(data) {
 	})
 }
 
-function Main(data) {
+function Main(data: any) {
 	return Roact.createFragment({
 		Status(data),
 		PlayerName(data),
@@ -192,6 +190,6 @@ const handle = Roact.mount(Main(data), parent)
 
 while (wait(1)) {
 	const data = access();
-	cost handle = Roact.update(handle, Main(data));
+	const handle = Roact.update(handle, Main(data));
 }
 
