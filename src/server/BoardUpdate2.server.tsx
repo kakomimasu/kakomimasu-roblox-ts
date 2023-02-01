88d904b02b80422c142fa5581c511429ce50e26b
ninja03 <69106571+ninja03@users.noreply.github.com>
2023-02-01 21:34:43 +0900

    Update BoardUpdate2.server.tsx

--- src/server/BoardUpdate2.server.tsx
+++ src/server/BoardUpdate2.server.tsx
@@ -1,25 +1,25 @@
 import { HttpService, Workspace } from "@rbxts/services";
-import Roact from '@rbxts/roact';
+import Roact from "@rbxts/roact";
 
 const FREE_COLOR = new Color3(1, 1, 1);
 
 const PLAYER = [
 	{
 		image: "rbxassetid://12194525464",
-		color: ["80C9FF", "0096FF"]
+		color: ["80C9FF", "0096FF"],
 	},
 	{
 		image: "rbxassetid://12194525688",
-		color: ["FE9998", "FF0200"]
+		color: ["FE9998", "FF0200"],
 	},
 	{
 		image: "rbxassetid://12194525849",
-		color: ["B8ABD6", "9370db"]
+		color: ["B8ABD6", "9370db"],
 	},
 	{
 		image: "rbxassetid://12194526006",
-		color: ["FFCF99", "ff8c00"]
-	}
+		color: ["FFCF99", "ff8c00"],
+	},
 ];
 
 const URL = "https://api.kakomimasu.com/v1/matches?sort=startedAtUnixTime&limit=1";
@@ -42,42 +42,42 @@ function Board(data: any) {
 	const height = data.board.height;
 	const xs = 1 / width;
 	const ys = 1 / height;
-	
+
 	BoardManager.update(data);
-	
+
 	const layout = Roact.createElement("UIGridLayout", {
 		FillDirectionMaxCells: width,
 		SortOrder: Enum.SortOrder.LayoutOrder,
 		CellSize: new UDim2(xs, 0, ys, 0),
-		CellPadding: new UDim2(0, 0, 0, 0) 
+		CellPadding: new UDim2(0, 0, 0, 0),
 	});
 	cells.push(layout);
-	
+
 	for (let y = 1; y < height; y++) {
 		for (let x = 1; x < width; x++) {
 			const n = (y - 1) * width + x;
 			const point = data.board.points[n];
 			const tile = data.tiled[n];
 			let playerImage, cellColor;
-			if (tile.player != null) {
+			if (tile.player != undefined) {
 				const player = PLAYER[tile.player + 1];
 				playerImage = player.image;
 				cellColor = Color3.fromHex(player.color[tile.type + 1]);
 			} else {
 				cellColor = FREE_COLOR;
 			}
-			
+
 			const cellContents = [];
-			
+
 			if (playerImage) {
 				const cellImage = Roact.createElement("ImageLabel", {
 					Image: playerImage,
 					BackgroundTransparency: 1,
-					Size: new UDim2(1, 0, 1, 0)
+					Size: new UDim2(1, 0, 1, 0),
 				});
 				cellContents.push(cellImage);
 			}
-			
+
 			const cellText = Roact.createElement("TextLabel", {
 				Text: point,
 				Size: new UDim2(1, 0, 1, 0),
@@ -85,29 +85,35 @@ function Board(data: any) {
 				TextSize: 9999,
 				BackgroundTransparency: 1,
 				TextXAlignment: Enum.TextXAlignment.Right,
-				TextYAlignment: Enum.TextYAlignment.Bottom
-			})
+				TextYAlignment: Enum.TextYAlignment.Bottom,
+			});
 			cellContents.push(cellText);
-			
-			const cell = Roact.createElement("Frame", {
-				BackgroundColor3: cellColor,
-				LayoutOrder: n
-			}, cellContents)
+
+			const cell = Roact.createElement(
+				"Frame",
+				{
+					BackgroundColor3: cellColor,
+					LayoutOrder: n,
+				},
+				cellContents,
+			);
 			cells.push(cell);
 		}
 	}
 
-	return Roact.createElement("Frame", {
+	return Roact.createElement(
+		"Frame",
+		{
 			Position: new UDim2(0, 0, 0, 400),
-			Size: new UDim2(1, 0, 1, -400)
-		}, [
-			Roact.createFragment(cells)
-		])
+			Size: new UDim2(1, 0, 1, -400),
+		},
+		[Roact.createFragment(cells)],
+	);
 }
 
 function Status(data: any) {
-	let text = ""
-	
+	let text = "";
+
 	if (data.gaming || data.ending) {
 		const player1 = data.players[1];
 		const player2 = data.players[2];
@@ -121,8 +127,8 @@ function Status(data: any) {
 		Size: new UDim2(1, 0, 0, 200),
 		Text: text,
 		TextScaled: true,
-		BackgroundColor3: new Color3(1, 1, 1)
-	})
+		BackgroundColor3: new Color3(1, 1, 1),
+	});
 }
 
 function getUser(userId: string) {
@@ -131,22 +137,21 @@ function getUser(userId: string) {
 		const response = HttpService.GetAsync(usersURL);
 		const json = HttpService.JSONDecode(response);
 		return json;
-	})
+	});
 	return result[1];
 }
 
-
 function PlayerName(data: any) {
 	let text = "";
-	
+
 	let player1Name = "";
 	let player2Name = "";
-	
+
 	if (data.gaming || data.ending) {
 		const player1 = data.players[1];
 		const player2 = data.players[2];
-		
-		if (player1.type == "account") {
+
+		if (player1.type === "account") {
 			// account player
 			const user1 = getUser(player1.id) as any;
 			player1Name = user1.screenName;
@@ -157,7 +162,7 @@ function PlayerName(data: any) {
 			player1Name = player1.id;
 			player2Name = player2.id;
 		}
-		
+
 		const point1 = player1.point.areaPoint + player1.point.wallPoint;
 		const point2 = player2.point.areaPoint + player2.point.wallPoint;
 		text = player1Name + "対" + player2Name;
@@ -168,16 +173,12 @@ function PlayerName(data: any) {
 		Size: new UDim2(1, 0, 0, 200),
 		Text: text,
 		TextScaled: true,
-		BackgroundColor3: new Color3(1, 1, 1)
-	})
+		BackgroundColor3: new Color3(1, 1, 1),
+	});
 }
 
 function Main(data: any) {
-	return Roact.createFragment([
-		Status(data),
-		PlayerName(data),
-		Board(data)
-	]);
+	return Roact.createFragment([Status(data), PlayerName(data), Board(data)]);
 }
 
 const data = access();
@@ -188,4 +189,3 @@ while (wait(1)) {
 	const data = access();
 	const handle = Roact.update(handle, Main(data));
 }
-
